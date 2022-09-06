dae1f05b3dab89138b5fa7f9d8476553fc907bcb
rogelio1502 <rogelio15021515@gmail.com>
2022-09-05 21:19:45 -0500

    Agregar archivos de proyecto.

---
+++ app-clase-1-demo/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using app_clase_1_demo.ViewModel;
+
+namespace app_clase_1_demo
+{
+    public partial class MainPage : ContentPage
+    {
+        LoginViewModel loginViewModel;
+        public MainPage()
+        {
+
+            InitializeComponent();
+            BindingContext = new LoginViewModel(this);
+            loginViewModel = BindingContext as LoginViewModel;
+
+            entryUsername.Completed += (object sender, EventArgs e) =>
+            {
+                entryPassword.Focus();
+            };
+
+            entryPassword.Completed += (object sender, EventArgs e) =>
+            {
+                loginViewModel.SubmitCommand.Execute(null);
+            };
+        }
+
+        
+}
+}
---
+++ app-clase-1-demo/ViewModel/LoginViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Xamarin.Forms;
+using System.Windows.Input;
+
+namespace app_clase_1_demo.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        ContentPage _page;
+
+        string _entryUsername;
+        string _entryPassword;
+
+        public string entryPassword
+        {
+            get { return _entryPassword; }
+            set
+            {
+                if (this._entryPassword != value)
+                {
+                    this._entryPassword = value;
+                    //notify that value changed
+                    PropertyChanged.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(
+                            nameof(this.entryPassword)
+                            )
+                        );
+                }
+            }
+        }
+        public string entryUsername
+        {
+            get
+            {
+                return _entryUsername;
+            }
+            set
+            {
+                if (this._entryUsername != value)
+                {
+                    this._entryUsername = value;
+                    //notify that value changed
+
+                    PropertyChanged.Invoke(
+                        this,
+                        new PropertyChangedEventArgs(
+                            nameof(this.entryUsername)
+                            )
+                        );
+                }
+            }
+        }
+
+        public ICommand SubmitCommand { protected set; get; }
+        public LoginViewModel(ContentPage page)
+        {
+            SubmitCommand = new Command(Login);
+            _page = page;
+        }
+
+
+        public async void Login()
+        {
+            if(entryUsername == "misterPro" && entryPassword == "holo15")
+            {
+                await _page.DisplayAlert("INFO", "Credenciales Correctas", "OK");
+                entryUsername = "";
+                entryPassword = "";
+
+            }
+            else
+            {
+                await _page.DisplayAlert("ERROR", "Credenciales Incorrectas", "OK");
+            }
+        }
+    }
+}
